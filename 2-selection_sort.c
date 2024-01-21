a8e1c716f4d6c7010b3c3ab7f220eec8454dcc20
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-01-21 01:05:30 +0100

    function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,42 +1,37 @@
 #include "sort.h"
 /**
- * swap - Function that swaps two values
- * @a: Fisrt value
- * @b: Second value
- * Return: 0
- */
-void swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *b;
-	*b = *a;
-	*a = tmp;
-}
-
-/**
- * selection_sort -  Selection sort algorithm
- * @array: Array to be sorted
- * @size: Size of array given
- * Return: 0
+ * selection_sort - function that sorts an array of integers
+ * from min to max
+ * @size: size of the array
+ * @array: list with numbers
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, min;
+	size_t i, j;
+	int temp = 0, sswap = 0, test = 0;
 
-	if (!array)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
-			if (array[j] < array[min])
-				min = j;
+		return;
+	}
 
-		if (i != min)
+	for (i = 0; i < size; i++)
+	{
+		temp = i;
+		test = 0;
+		for (j = i + 1; j < size; j++)
 		{
-			swap(&array[min], &array[i]);
+			if (array[temp] > array[j])
+			{
+				temp = j;
+				test += 1;
+			}
+		}
+		sswap = array[i];
+		array[i] = array[temp];
+		array[temp] = sswap;
+		if (test != 0)
+		{
 			print_array(array, size);
 		}
 	}
